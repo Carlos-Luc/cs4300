91322aa4b5a4bfb750459fcfab79be6606f35400
Carlos-Luc <carloslucero232@gmail.com>
2026-09-17 01:17:38 +0000

    Started on tasks

---
+++ Homework1/src/task_2.py
@@ -0,0 +1,31 @@
+positive = 4
+negative = -3
+float_1 = 3.14
+float_2 = 6.28
+a_sentence = "This is a sentence."
+boolean_1 = True
+boolean_2 = False
+
+#Addition(int)
+
+print(positive + negative)
+
+#Subtraction(int)
+
+print(positive - negative)
+
+#Addition(float)
+
+print(float_1 + float_2)
+
+#Subtraction(float)
+
+print(float_1 - float_2)
+
+#String slicing
+
+print(a_sentence[10::])
+
+print(f"If true and false then {boolean_1 and boolean_2}")
+
+print(f"If true or false then {boolean_1 or boolean_2}")
---
+++ Homework1/src/task_3.py
@@ -0,0 +1,13 @@
+def sign_check(num):
+
+    if(num > 0):
+
+        return "positive"
+    
+    elif(num < 0):
+
+        return "negative"
+    
+    else:
+
+        return "zero"
